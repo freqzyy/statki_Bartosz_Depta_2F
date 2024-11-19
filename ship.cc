7fe09a3d6fdda03349b2f4b54f11205c823b83f6
freqzyy <bartekdepta1234@gmail.com>
2024-11-19 21:58:09 +0100

    Update ship.cc

--- ship.cc
+++ ship.cc
@@ -3,17 +3,9 @@
 Ship::Ship()
 {
 }
+
+// Konstruktor statku
 Ship::Ship(int len)
 {
     length_of_ship = len;
-}
-
-// Dodaje pozycje statku
-void Ship::addPos(int index, int x, int y)
-{
-    if (index < length_of_ship)
-    {
-        x_pos[index] = x;
-        y_pos[index] = y;
-    }
 }
