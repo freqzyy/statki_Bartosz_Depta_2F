1cf5d5a45a25106f57ab3e99f0c932c222ca18ff
freqzyy <bartekdepta1234@gmail.com>
2024-12-03 19:29:22 +0100

    Update player.cc

--- player.cc
+++ player.cc
@@ -7,6 +7,11 @@ Player::Player()
 {
 }
 
+void Player::getName(Player &currentplayer)
+{
+    cin >> currentplayer.name;
+}
+
 // Dodanie statkow graczom
 
 void Player::AddShips()
@@ -32,17 +37,17 @@ void Player::setupShips(Player &currentplayer)
         {
             getShipPlacement(x, y, direction);
             placed = ownBoard.placeship(ships[i], x, y, direction);
-            if (placed == false)
+            if (!placed)
             {
-                cout << "Nie mozna umiescic tutaj statku, sprobuj ponownie." << endl;
+                cout << "Nie mozna umiescic tutaj statku, statek wystawalby poza plansze lub stykalby sie z innym statkiem" << endl;
                 currentplayer.ownBoard.display(false);
             }
             else
             {
-                cout << "Statek pomyslnie ustawiony na pozycji: (" << y << ", " << x << ")" << " z kierunkiem: " << direction << endl;
+                cout << "Statek pomyslnie ustawiony na pozycji: (" << x << ", " << y << ")" << " z kierunkiem: " << direction << endl;
             }
         }
-        while (placed == false);
+        while (!placed);
     }
 }
 
@@ -52,31 +57,9 @@ void Player::getShipPlacement(int &x, int &y, char &direction)
 {
     bool validInput = false;
 
-    while (validInput == false)
+    while (!validInput)
     {
         cout << "Podaj wiersz(0-" << size - 1 << "): ";
-        cin >> y;
-        if (cin.fail())
-        {
-            cout << "Podaj liczbe calkowita" << endl;
-            cin.clear();
-            cin.ignore(numeric_limits <streamsize>::max(), '\n');
-        }
-        else if (y < 0)
-        {
-            cout << "Podana liczba jest mniejsza od zera" << endl;
-        }
-        else
-        {
-            validInput = true;
-        }
-    }
-
-    validInput = false;
-    
-    while (validInput == false)
-    {
-        cout << "Podaj kolumne(0-" << size - 1 << "): ";
         cin >> x;
         if (cin.fail())
         {
@@ -88,6 +71,36 @@ void Player::getShipPlacement(int &x, int &y, char &direction)
         {
             cout << "Podana liczba jest mniejsza od zera" << endl;
         }
+        else if(x >= size)
+        {
+            cout << "Podana liczba jest wieksza od rozmiaru planszy" << endl;
+        }
+        else
+        {
+            validInput = true;
+        }
+    }
+
+    validInput = false;
+    
+    while (!validInput)
+    {
+        cout << "Podaj kolumne(0-" << size - 1 << "): ";
+        cin >> y;
+        if (cin.fail())
+        {
+            cout << "Podaj liczbe calkowita" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits <streamsize>::max(), '\n');
+        }
+        else if (y < 0)
+        {
+            cout << "Podana liczba jest mniejsza od zera" << endl;
+        }
+        else if(y >= size)
+        {
+            cout << "Podana liczba jest wieksza od rozmiaru planszy" << endl;
+        }
         else
         {
             validInput = true;
@@ -96,7 +109,7 @@ void Player::getShipPlacement(int &x, int &y, char &direction)
 
     validInput = false;
 
-    while (validInput == false)
+    while (!validInput)
     {
         cout << "Podaj kierunek (h - poziomo,  v - pionowo): ";
         cin >> direction;
@@ -140,8 +153,8 @@ bool Player::PlrShoot(Player &opponent, int x, int y)
 
     do
     {
-        cout << "Podaj wspolrzedne strzalu(x,y):";
-        cin >> y >> x;
+        cout << "Podaj wspolrzedne strzalu(wiersz - (0-9),kolumna - (0-9)):";
+        cin >> x >> y;
         if (cin.fail() || x < 0 || x >= size || y < 0 || y >= size)
         {
             cout << "Niepoprawne wspolrzedne" << endl;
@@ -152,7 +165,7 @@ bool Player::PlrShoot(Player &opponent, int x, int y)
 
         validInput = true;
     } 
-    while (validInput == false);
+    while (!validInput);
 
     bool hit = opponent.ownBoard.shoot(x,y);
     if (hit)
